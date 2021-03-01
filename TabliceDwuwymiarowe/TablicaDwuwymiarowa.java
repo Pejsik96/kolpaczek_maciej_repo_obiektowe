e5806c34f8beb84bd04a41fed62432ab97b74156
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-03-01 09:27:49 +0100

    przeniesienie tablicy dwuwymiarowej

--- TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
+++ TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
@@ -1,12 +1,9 @@
 
 class TablicaDwuwymariowa{
     public static void main(String[] args){
-        int [] tab = new int[5];
-        tab[0]=40;
-        //tab[1]="Jeden";
-        for(int i=0; i<tab.length; i++){
-            System.out.println(tab[i]);
-        }
+
+    }
+    private void tab2dFirst(){
 
         int [][] tab2d=new int[5][2];
 
