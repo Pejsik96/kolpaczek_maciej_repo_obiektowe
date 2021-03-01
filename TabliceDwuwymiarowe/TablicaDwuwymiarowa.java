80b02fbe77afe607301be28250d611b156d286fb
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-03-01 09:08:32 +0100

    stworzenie tabilcy dwuwymiarowej

--- TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
+++ TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
@@ -3,6 +3,12 @@ class TablicaDwuwymariowa{
     public static void main(String[] args){
         int [] tab = new int[5];
         tab[0]=40;
-        tab[1]="Jeden";
+        //tab[1]="Jeden";
+        for(int i=0; i<tab.length; i++){
+            System.out.println(tab[i]);
+        }
+
+        int [][] tab2d=new int[5][2];
+        
     }
 }
