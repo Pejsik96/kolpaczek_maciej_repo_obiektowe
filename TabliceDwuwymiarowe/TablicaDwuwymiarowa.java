6a1f26d79028f23ec25dedcfdf778670450ef20a
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-03-01 09:25:43 +0100

    wystwietlenie tablicy dwuwymiarowej

--- TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
+++ TabliceDwuwymiarowe/TablicaDwuwymiarowa.java
@@ -9,6 +9,16 @@ class TablicaDwuwymariowa{
         }
 
         int [][] tab2d=new int[5][2];
-        
+
+        tab2d[0][1]=50;
+        System.out.println(tab2d.length);
+        System.out.pritln(tab2d.length*tab2d[0].length);
+        for(int i=0; i<tab2d.length; i++){
+            for (int j=0; j<tab2d[i].length; j++){
+                System.out.println(tab2d[i][j]);
+            }
+            System.out.println("Koniec tepli z mienna j");
+        }
+        System.out.println("Koniec petli z zmienna i");
     }
 }
