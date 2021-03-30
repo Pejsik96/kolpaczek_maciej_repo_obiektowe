9087a2139c6a4b313dc8b63e47cbdd6a634a6372
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-03-30 09:24:17 +0200

    Zadanie 1 30.03.2021

---
+++ CwiczeniaObiektowe_1.java/CwiczenieObiektowe_1.java
@@ -0,0 +1,6 @@
+
+class CwiczenieObiektowe_1{
+    public static void main (String[] args){
+        MojeDane md=new MojeDane("Maciej","Kolpaczek","2PTN");
+    }
+}
---
+++ CwiczeniaObiektowe_1.java/MojeDane.java
@@ -0,0 +1,13 @@
+
+class MojeDane{
+    public String imie=" ";
+    public String nazwisko=" ";
+    public String klasa=" ";
+
+    public MojeDane(String imie, String nazwisko, String klasa){
+        this.imie=imie;
+        this.nazwisko=nazwisko;
+        this.klasa=klasa;
+        System.out.println("Witaj "+imie+" "+nazwisko+" "+klasa);
+    }
+}
