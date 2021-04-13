edd77461275efecfdb222be2904758dbc265c75d
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-04-13 10:15:47 +0200

    commit dla plana

--- TrojkatyZadania/Main.java
+++ TrojkatyZadania/Main.java
@@ -1,6 +1,8 @@
 class Main{
     public static void main(String[] args){
-        Triangle tri = new Triangle(10,4);
+        Triangle tri = new Triangle(3,5);
+        System.out.println(tri);
 
+        System.out.println();
     }
 }
