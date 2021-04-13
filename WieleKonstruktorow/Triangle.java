5286a4cd75ad3975c30bac532f9cd0af03bbce1f
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-04-13 09:49:28 +0200

    niedziala

--- WieleKonstruktorow/Triangle.java
+++ WieleKonstruktorow/Triangle.java
@@ -1,10 +1,17 @@
 
 class Triangle{
 
-    private int a=0;
-    private int h=0;
+    private int a;
+    private int h;
+
+    public Triangle(int a,int h){
+        this.a = a;
+        this.h = h;   
+    }
+
+
     
-    public Triangle(int h, int a){
+    public int PoleTriangle(){
         int Pole = h*a/2;
         System.out.println("Pole : "+ Pole);
         return Pole;
