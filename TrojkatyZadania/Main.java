5c1f0ace1f8a25970871979432e9febd07abe861
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-04-13 15:42:26 +0200

    uzupelnienie zadania

--- TrojkatyZadania/Main.java
+++ TrojkatyZadania/Main.java
@@ -1,6 +1,8 @@
 class Main{
     public static void main(String[] args){
-        Triangle tr = new Triangle(3, 5);
-
+        Triangle tr1 = new Triangle(12,4);
+        Triangle tr2 = new Triangle(6);
+        Triangle tr3 = new Triangle(3,12,5,8);
+        //read me Triangle.java
     }
 }
--- TrojkatyZadania/Triangle.java
+++ TrojkatyZadania/Triangle.java
@@ -3,6 +3,10 @@ class Triangle {
         System.out.println("Pole trojkata "+wysokosc*podstawa/2);
     }
     public Triangle(int podstawa){
-        System.out.println("Rownoboczny "+(Math.pow(podstawa, 2)*Math.pow(podstawa, 1/3))/4);
+        System.out.println("Rownoboczny "+(podstawa*podstawa*Math.sqrt(3)/4));
     }
+    public Triangle(int a, int b, int c, int r){
+        System.out.println("Pole trojkata "+(a*b*c/4*r));
+    }
+    //Reszte konstruktorow nie zrobimy bez znajomosci sinusow etc oraz poniewaz juz istnieja takie konstruktory
 }
