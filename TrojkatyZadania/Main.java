a7aa5fcf7ade06ae77809215d68a38ee2f7decbe
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2021-04-13 09:44:52 +0200

    AAA

--- TrojkatyZadania/Main.java
+++ TrojkatyZadania/Main.java
@@ -1,6 +1,7 @@
 
 class Main{
     public static void main(String[] args){
-
+        Triangle T1 = new Triangle(12,8);
+        System.out.println(T1);
     }
 }
--- WieleKonstruktorow/Triangle.java
+++ WieleKonstruktorow/Triangle.java
@@ -1,4 +1,12 @@
 
 class Triangle{
+
+    private int a=0;
+    private int h=0;
     
+    public Triangle(int h, int a){
+        int Pole = h*a/2;
+        System.out.println("Pole : "+ Pole);
+        return Pole;
+    }
 }
