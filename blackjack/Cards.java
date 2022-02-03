8aae497f630b473a13643fe197dc57f6428e6be4
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2022-02-03 11:27:16 +0100

    commit sprawdzajacy 1

---
+++ blackjack/Cards.java
@@ -0,0 +1,9 @@
+public class Cards {
+    private String name;
+    private int value;
+
+    public Cards(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+}
