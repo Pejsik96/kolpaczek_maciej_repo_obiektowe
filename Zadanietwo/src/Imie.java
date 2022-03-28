c4d39df05e60d431892c77366cef266324895380
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2022-03-28 09:32:28 +0200

    ZABIJE SIE ZABIJE SIE ZABIJE SIE ZABIJE SIE ZABIJE

---
+++ Zadanietwo/src/Imie.java
@@ -0,0 +1,12 @@
+public class Imie implements Zabijesie{
+    @Override
+    public String imiona(){
+
+        return null;
+    }
+
+    @Override
+    public int wiek() {
+        return 0;
+    }
+}
---
+++ Zadanietwo/src/Zabijesie.java
@@ -0,0 +1,4 @@
+public interface Zabijesie {
+    String imiona();
+    int wiek();
+}
