a89a6e7b11c82fa752841f3d8238d05ca38c8744
Pejsik96 <77607540+Pejsik96@users.noreply.github.com>
2022-04-03 14:28:25 +0200

    podjebane

--- Zadaniefive/Rzeczy.java
+++ Zadaniefive/Rzeczy.java
@@ -5,29 +5,42 @@ import java.util.Scanner;
 public class Rzeczy implements Xdddd {
     @Override
     public int dninaswiecie(){
-        Scanner userInput = new Scanner(System.in);
-    
-        int todayYear = Calendar.getInstance().get(Calendar.YEAR);
-        int todayMonth = Calendar.getInstance().get(Calendar.MONTH);
-        todayMonth+=1;
+        Scanner scanner = new Scanner(System.in);
+
         int todayDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        System.out.println("Dzisiejsza data to: "+todayDay+"-"+todayMonth+"-"+todayYear);
-    
-        Calendar today = new GregorianCalendar(todayYear, todayMonth,
-                todayDay);
-    
+        int todayMonth = Calendar.getInstance().get(Calendar.MONTH)+1;
+        int todayYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        Calendar today = new GregorianCalendar(todayYear, todayMonth, todayDay);
+
         System.out.println("Podaj date urodzenia: ");
-        System.out.print("Rok: ");
-        int yearBorn = userInput.nextInt();
-        System.out.print("Miesiac: ");
-        int monthBorn = userInput.nextInt();
-        System.out.print("Dzien: ");
-        int dayBorn = userInput.nextInt();
-        Calendar born = new GregorianCalendar(yearBorn, monthBorn, dayBorn);
-    
+        String data = scanner.next();
+        String[] format = data.split("-");
+
+        String l1 = format[0];
+        String l2 = format[1];
+        String l3 = format[2];
+
+        int birthDay = 0;
+        int birthMonth = 0;
+        int birthYear = 0;
+
+        if(l1.length() == 2 && l2.length() == 2 && l3.length() == 4){
+            birthDay = Integer.parseInt(l1);
+            birthMonth = Integer.parseInt(l2);
+            birthYear = Integer.parseInt(l3);
+        }else if (l1.length() == 4 && l2.length() == 2 && l3.length() == 2){
+            birthDay = Integer.parseInt(l3);
+            birthMonth = Integer.parseInt(l2);
+            birthYear = Integer.parseInt(l1);
+        }
+        
+        Calendar born = new GregorianCalendar(birthYear, birthMonth, birthDay);
+        
         double roznica = today.getTimeInMillis() - born.getTimeInMillis();
         roznica = roznica/(24*60*60*1000);
-        System.out.println(Math.round(roznica+1));
+        System.out.println(Math.round(roznica));
+    
         return 0;
     }
 }
